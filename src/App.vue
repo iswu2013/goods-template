db987f4afde544fc9fd681ffd60e03ec5f44f37a
1037216275@qq.com <1037216275@qq.com>
2017-02-03 11:42:26 +0800

    商品模板架构初次提交

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+      <div>
+        I am header
+      </div>
+    <div>
+      I am tab
+    </div>
+    <div>
+      I am content
+    </div>
+  </div>
+</template>
+<script>
+</script>
+<style>
+</style>
